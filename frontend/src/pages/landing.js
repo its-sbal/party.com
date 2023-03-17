87866ede4f2b4cabcf007e2a550555fc39c00743
HAPPY <cse.190310218@silicon.ac.in>
2023-03-17 16:52:08 +0530

    added landing input

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,11 +1,29 @@
 import "./landing.css";
+import React from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 import photo1 from "../images/landing1.png";
+import { Button, Space } from "antd";
 function Landing() {
   return (
     <div>
-      <div className="left"></div>
+      <div className="left">
+        <div className="insideleft">
+          <div>Name</div>
+          <Input placeholder="Your Name" prefix={<UserOutlined />} />
+          <div>Number</div>
+          <Input placeholder="Your Number" prefix={<UserOutlined />} />
+          <div>Location</div>
+          <Input placeholder="Your Location" prefix={<UserOutlined />} />
+          <div className="buttonin">
+            <Button type="primary" danger>
+              Submit
+            </Button>
+          </div>
+        </div>
+      </div>
       <div className="right">
-        <img src={photo1} style={{ height: "1000px", width: "100%" }} />
+        <img src={photo1} style={{ height: "110%", width: "100%" }} />
       </div>
     </div>
   );
