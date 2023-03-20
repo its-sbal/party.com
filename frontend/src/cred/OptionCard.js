5c3095142a3a0b712d327d5929bb4bf955b85ec3
SBal <saubhagyabal2001@gmail.com>
2023-03-20 19:36:04 +0530

    d

--- frontend/src/cred/OptionCard.js
+++ frontend/src/cred/OptionCard.js
@@ -11,7 +11,8 @@ function Card(props) {
     <div className="card-main">
       <Column>
         <div className="option-image">
-          <img src={image1} style={{ width: "150px", height: "45%" }} />
+          {/* <img src={image1} style={{ width: "150px", height: "45%" }} /> */}
+          {/* <iframe src="https://embed.lottiefiles.com/animation/90504"></iframe> */}
         </div>
         <div className="option-card">
           <div className="option-heading">{props.name}</div>
