0d36061e14f4818c2dcb56ddb6215eff3fd40e29
SBal <saubhagyabal2001@gmail.com>
2023-03-23 16:33:03 +0530

    3

--- frontend/src/cred/OptionCard.js
+++ frontend/src/cred/OptionCard.js
@@ -2,25 +2,34 @@ import "./OptionCard.css";
 import { Button, Typography, Column } from "@cred/neopop-web/lib/components";
 import { FontVariant } from "@cred/neopop-web/lib/primitives";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Tilt from "react-parallax-tilt";
 
 import Birthday from "../pages/events/birthdayEvent";
 import image1 from "../images/wedding.png";
 
 function Card(props) {
   return (
-    <div className="card-main" style={{ backgroundColor: props.color }}>
-      <Column>
-        <div className="option-image">
-          <img src={props.image} className="image" />
-        </div>
-        <div className="option-card">
-          <div className="option-heading">{props.name}</div>
-          <Link to="/birthday">
-            <div className="view-button"></div>
-          </Link>
-        </div>
-      </Column>
-    </div>
+    <Tilt
+      glareEnable={false}
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      perspective={500}
+      glareColor={"rgb(255,0,0,0)"}
+    >
+      <div className="card-main" style={{ backgroundColor: props.color }}>
+        <Column>
+          <div className="option-image">
+            <img src={props.image} className="image" />
+          </div>
+          <div className="option-card">
+            <div className="option-heading">{props.name}</div>
+            <Link to="/birthday">
+              <div className="view-button"></div>
+            </Link>
+          </div>
+        </Column>
+      </div>
+    </Tilt>
   );
 }
 
