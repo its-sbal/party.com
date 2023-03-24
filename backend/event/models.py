2a332d65bf7bcfb7cdff603edf22d5d5caac4fe9
HAPPY <cse.190310218@silicon.ac.in>
2023-03-24 19:52:16 +0530

    backend

--- backend/event/models.py
+++ backend/event/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class user(models.Model):
+    _id = models.AutoField(primary_key=True)
+    fName = models.CharField(max_length=100,null=True,blank=True)
+    lName = models.CharField(max_length=100,null=True,blank=True)
+    email = models.CharField(max_length=100,null=True,blank=True)
+    password = models.CharField(max_length=100,null=True,blank=True)
+    address = models.TextField(max_length=100,null=True,blank=True)
+    mobile = models.IntegerField(null=True,blank=True,validators=[MinValueValidator(1),MaxValueValidator(10)])
+
+    def __str__(self):
+        return self.fname
+    
+class vendor(models.Model):
+    _id = models.AutoField(primary_key=True)
+    vendorName = models.CharField(max_length=100,null=True,blank=True)
+    service = models.CharField(max_length=100,null=True,blank=True)
+    numReview = models.IntegerField(null=True,blank=True)
+    rating = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1),MaxValueValidator(10)])
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(null=True,blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    email = models.CharField(max_length=100,null=True,blank=True)
+    password = models.CharField(max_length=100,null=True,blank=True)
+    mobile = models.IntegerField(null=True,blank=True)
+    address = models.TextField(max_length=100,null=True,blank=True)
+
+    def __str__(self):
+        return self.vendorName
+
+class orders(models.Model):
+    _id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField(null=True,blank=True)
+    bookFrom = models.DateField(auto_now_add=False)
+    bookUpto = models.DateField(auto_now_add=False)
+    isPaid = models.BooleanField(default=False)
+    totalPrice = models.IntegerField(null=True,blank=True,validators=[MinValueValidator(1),MaxValueValidator(10)])
