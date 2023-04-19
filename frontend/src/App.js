812a6e30a3a6969aec833ee7d198447ecc41150c
Priyadarshi <prdrshsatyaprakash@gmail.com>
2023-04-19 23:29:43 +0530

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import About from "./pages/navBarItems/about";
 import HomeMain from "./components/HomeMain";
 import Landing from "./pages/login/landing";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/signin" element={<Signin />} />
         </Routes>
       </Router>
+      <Footer />
     </>
   );
 }
