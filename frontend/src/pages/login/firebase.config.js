629bcbc9a1247cfac9aff73cca9ceffa128afd94
HAPPY <cse.190310218@silicon.ac.in>
2023-04-26 19:42:50 +0530

    vendor login

---
+++ frontend/src/pages/login/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-b-5ayuqsluTh6HNH31-iUGMey566FX0",
+  authDomain: "otp-project-f52b2.firebaseapp.com",
+  projectId: "otp-project-f52b2",
+  storageBucket: "otp-project-f52b2.appspot.com",
+  messagingSenderId: "104245197541",
+  appId: "1:104245197541:web:2fbc118cb8ad81f18cb20c",
+  measurementId: "G-1RY31WHF47",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
