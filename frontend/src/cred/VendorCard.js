d0c6857958760993611bb0506511cd38d5c7eae3
Priyadarshi <prdrshsatyaprakash@gmail.com>
2023-04-27 14:23:39 +0530

    Change done

--- frontend/src/cred/VendorCard.js
+++ frontend/src/cred/VendorCard.js
@@ -1,5 +1,21 @@
-function VendorCard({ product }) {
-  return <div className="vname">{product.price}</div>;
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Card, Button, Row } from "react-bootstrap";
+import img1 from "./hi1.jpg";
 
+function VendorCard({ product }) {
+  return (
+    <Container className="p-4">
+      <Row md="4">
+        <Card>
+          <Card.Img variant="top" src={img1} />
+          <Card.Body>
+            <Card.Title>{product.vendorName}</Card.Title>
+            <Card.Text>{product.price}</Card.Text>
+            {/* <Button variant="primary">Add to cart</Button> */}
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  );
+}
 export default VendorCard;
