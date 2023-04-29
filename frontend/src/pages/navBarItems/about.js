154da4fa79d2b68714762d7c4136dcaf9fdd660c
HAPPY <cse.190310218@silicon.ac.in>
2023-04-29 12:10:58 +0530

    about us page

--- frontend/src/pages/navBarItems/about.js
+++ frontend/src/pages/navBarItems/about.js
@@ -1,13 +1,14 @@
 import React from "react";
 import img1 from "./photo default.jpg";
 import "./about.css";
+import img2 from "./happu.png";
 
 function About() {
   return (
     <div>
       <div className="about-section">
-        <h1>About Us</h1>
-        <p>
+        <h1 className="odi2">About Us</h1>
+        <p className="odi2">
           Welcome to our party and event organizing app! We are a team of
           passionate event planners who understand the importance of creating
           memorable experiences for our clients. With years of experience in the
@@ -25,12 +26,12 @@ function About() {
       <div className="row">
         <div className="col-sm">
           <div className="card">
-            <img src={img1} alt="Jane" style={{ width: "100%" }} />
+            <img src={img2} alt="Jane" style={{ width: "80%" }} />
             <div className="container">
               <h2>Sudhansu</h2>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>jane@example.com</p>
-              <p>
+              <p></p>
+              <p>sitsekhar@example.com</p>
+              <p className="odi1">
                 <button className="button">Contact</button>
               </p>
             </div>
@@ -38,12 +39,12 @@ function About() {
         </div>
         <div className="col-sm">
           <div className="card">
-            <img src={img1} alt="Mike" style={{ width: "100%" }} />
+            <img src={img1} alt="sudhansu" style={{ width: "100%" }} />
             <div className="container">
               <h2>Soubhagya</h2>
               <p>Some text that describes me lorem ipsum ipsum lorem.</p>
               <p>mike@example.com</p>
-              <p>
+              <p className="odi1">
                 <button className="button">Contact</button>
               </p>
             </div>
@@ -57,7 +58,7 @@ function About() {
               <h2>Priyadarshi</h2>
               <p>Some text that describes me lorem ipsum ipsum lorem.</p>
               <p>john@example.com</p>
-              <p>
+              <p className="odi1">
                 <button className="button">Contact</button>
               </p>
             </div>
@@ -71,7 +72,7 @@ function About() {
               <h2>Ayush Kumar</h2>
               <p>Some text that describes me lorem ipsum ipsum lorem.</p>
               <p>mike@example.com</p>
-              <p>
+              <p className="odi1">
                 <button className="button">Contact</button>
               </p>
             </div>
