cf5747caabd81f294bbc7b7510eeb57fa406ecb1
Sbal <saubhagyabal2001@gmail.com>
2023-05-04 12:13:32 +0530

    s

--- frontend/src/cred/ConcertCard.js
+++ frontend/src/cred/ConcertCard.js
@@ -102,6 +102,34 @@ const ConcertCard = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         }
       />
+      <Card
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header={<h1>Lakes</h1>}
+        content={
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        }
+      />
+      <Card
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header={<h1>Lakes</h1>}
+        content={
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        }
+      />
+      <Card
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header={<h1>Lakes</h1>}
+        content={
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        }
+      />
+      <Card
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header={<h1>Lakes</h1>}
+        content={
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        }
+      />
     </div>
   );
 };
