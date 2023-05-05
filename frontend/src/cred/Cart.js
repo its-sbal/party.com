428c2abdaf7519463ff370b3e5373800c2543e9f
Sbal <saubhagyabal2001@gmail.com>
2023-05-05 18:11:56 +0530

    ss

--- frontend/src/cred/Cart.js
+++ frontend/src/cred/Cart.js
@@ -8,6 +8,24 @@ function Cart() {
   cartItems.forEach((item) => {
     totalPrice += parseInt(item.price);
   });
+
+  //adding id of vendor to array===========
+  // Extract the values from cartItems and store them in an array
+  // const myArrayFieldValues = cartItems.map((item) => item._id);
+
+  // fetch("http://127.0.0.1:8000/userUpdate/", {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json, text/plain, */*",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //   .then((response) => {
+  //     // Handle the response here
+  //     console.log(response.data);
+  //   });
+
   return (
     <div>
       <h2>Cart</h2>
